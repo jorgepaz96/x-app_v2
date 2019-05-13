7e3c40a8d9a0fbe0c95b6888e96a70b42e259926
jorgepaz96 <pazmissael@gmail.com>
2019-05-13 17:17:51 -0500

    cambios realizados

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,12 +1,23 @@
 # services/users/manage.py
 
 import unittest
+import coverage
 
 
 from flask.cli import FlaskGroup
 from project import create_app, db
 
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app = create_app) 
@@ -33,6 +44,20 @@ def test():
         return 0
     return 1
 
+@cli.command()
+def cov():
+    """Ejecuta las pruebas unitarias con coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Resumen de cobertura:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
 
 
 
